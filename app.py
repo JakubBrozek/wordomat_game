607d3b6fc21654a3dd719d22cd3299f16e289cc6
jakub Brożek <jakub.brozek17@gmail.com>
2024-09-10 23:32:05 +0200

    It's usefull to have alphabet available

--- app.py
+++ app.py
@@ -11,6 +11,11 @@ class Dictionary:
             print("Lista słów jest pusta ")
             return
         return self.dictionary
+    def get_alphabet(self):
+        alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+        self.alphabet = alphabet
+
+        return alphabet
 
 def enemy_move(words):
     while True:
